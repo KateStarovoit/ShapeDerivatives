c476befe0f0e4e6ffbd8cd17477c5702c8f5964f
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-07 22:37:49 -0500

    Create cotmatrix_directional_derivative.py

---
+++ kates_derivatives_draft/cotmatrix_directional_derivative.py
@@ -0,0 +1,17 @@
+#directional derivative for cot matrix
+def cotmatrix_directional_derivative(v, f, d):
+    L_grad = np.zeros((len(v),(len(v))))
+    dcotijdx = l_grad_entries_di(v,f,0)
+    dcotijdy = l_grad_entries_di(v,f,1)
+    dcotijdz = l_grad_entries_di(v,f,2)
+    dcotij = [dcotijdx , dcotijdy ,dcotijdz]
+    
+    for i in range(len(f)):
+        for j in range(3):
+            for k in range(3):
+               entry = dcotij[k][i][j]*d[i][j][k]
+               L_grad[f[i][(j+1)%3]][f[i][(j+2)%3]]+= entry
+               L_grad[f[i][(j+2)%3]][f[i][(j+1)%3]]+= entry
+               L_grad[f[i][(j+1)%3]][f[i][(j+1)%3]]-= entry
+               L_grad[f[i][(j+2)%3]][f[i][(j+2)%3]]-= entry
+    return L_grad
