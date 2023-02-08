82322fdf16435c1f18158eb8acb4abc24fb89301
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-07 21:53:10 -0500

    Create l_grad_entries.py

---
+++ kates_derivatives_draft/l_grad_entries.py
@@ -0,0 +1,20 @@
+import igl
+import numpy as np
+
+#l_grad for one triangle
+def grad_li(v,A_i):
+    grad = [np.zeros((3,3)), np.zeros((3,3)), np.zeros((3,3))]
+    for i in range(len(grad)):
+        for j in range(0,3):
+            grad[i][j][0] = (2*v[i%3][j]-v[(i+1)%3][j]-v[(i+2)%3][j])/(4*A_i)
+            grad[i][j][1] = (v[(i+2)%3][j]-v[i%3][j])/(4*A_i)
+            grad[i][j][2] = (-v[(i+1)%3][j]-v[i%3][j])/(4*A_i)
+    return grad
+
+#l_grad entries for each face
+def l_grad_entries(v,f):
+    L_grad_entries = []
+    A = igl.doublearea(v,f)
+    for i in range(len(f)):
+        L_grad_entries.append(grad_li([v[f[i][0]],v[f[i][1]],v[f[i][2]]],A[i]))
+    return L_grad_entries
