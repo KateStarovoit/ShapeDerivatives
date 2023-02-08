f4a39861478d97398fa7b8f969e3a3e3185cbf81
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-07 22:06:41 -0500

    Update l_grad_entries.py

--- kates_derivatives_draft/l_grad_entries.py
+++ kates_derivatives_draft/l_grad_entries.py
@@ -18,3 +18,14 @@ def l_grad_entries(v,f):
     for i in range(len(f)):
         L_grad_entries.append(grad_li([v[f[i][0]],v[f[i][1]],v[f[i][2]]],A[i]))
     return L_grad_entries
+
+#returns n*n l_grad_entries matrix
+def l_grad_entries_assembled(v,f):
+    cot_entries = l_grad_entries(v,f)
+    L_grad_assembled = np.zeros((len(f),3))
+    for i in range(len(cot_entries)):
+        for j in range(len(cot_entries[i])):
+            for k in range(len(cot_entries[i][j])):
+                for l in range(len(cot_entries[i][j][k])):
+                      L_grad_assembled [i][j] += cot_entries[i][j][k][l] 
+    return L_grad_assembled 
