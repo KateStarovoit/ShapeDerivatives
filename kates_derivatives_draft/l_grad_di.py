d1c5b6b61bdf82908e0c5234724d4d959539172f
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-09 09:10:16 -0500

    Update l_grad_di.py

--- kates_derivatives_draft/l_grad_di.py
+++ kates_derivatives_draft/l_grad_di.py
@@ -1,4 +1,12 @@
 #l_grad in one direction(dx, dy or dz)
+'''
+Input
+V - n x 3 vertex positions
+F - m x 3 faces
+di - scalar direction. 0 for x, 1 for y and 2 for z
+Output
+n x n l_grad in one direction
+'''
 def l_grad_di(v,f,di):
     L_grad = np.zeros((len(v),(len(v))))
     cot_entries = l_grad_entries_di(v,f,di)
