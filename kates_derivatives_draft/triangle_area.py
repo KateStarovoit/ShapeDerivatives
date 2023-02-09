6c89825cd42b116d4b5bc1f98fa0dc95ff131ab9
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-08 20:59:27 -0500

    Update triangle_area.py

--- kates_derivatives_draft/triangle_area.py
+++ kates_derivatives_draft/triangle_area.py
@@ -2,6 +2,10 @@ import torch
 import numpy as np
 
 #Area of one triangle
+#Input
+# V - 3 x 3 tensor of vertex positions
+#Output
+# a - traingle area
 def triangle_area(V):
     s1 = torch.linalg.norm(V[0]-V[1])
     s2 = torch.linalg.norm(V[1]-V[2])
@@ -9,10 +13,3 @@ def triangle_area(V):
     p = (s1+s2+s3)/2
     a = torch.sqrt(p*(p-s1)*(p-s2)*(p-s3))
     return a
-    
-#Triangle area for shape
-def triangle_area(v, f):
-    A = np.zeros(len(f))
-    for i in range(len(f)):
-         A[i] = torch.tensor(f[i])
-    return A
