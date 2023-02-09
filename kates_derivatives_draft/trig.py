83b119ef0c3e44be6a4886c69eacf8cacc30cde7
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-08 20:32:48 -0500

    Update trig.py

--- kates_derivatives_draft/trig.py
+++ kates_derivatives_draft/trig.py
@@ -1,16 +1,32 @@
 #Trigonometric functions for one triangle
+
+#Input
+#a, b, c - n x 3 vertex positions
+#Ai - area of corresponding triangle
+#Output
+#sin for each angle
 def find_sin(a, b, c, Ai):
     sina = (Ai)/(b*c)
     sinb = (Ai)/(a*c)
     sinc = (Ai)/(a*b)
     return sina, sinb, sinc
 
+#Input
+#a, b, c - n x 3 vertex positions
+#Ai - area of corresponding triangle
+#Output
+#cos for each angle
 def find_cos(a, b, c):
     cosa = (- a * a + b * b + c * c) / (2 * b * c)
     cosb = (- b * b + a * a + c * c) / (2 * a * c)
     cosc = (- c * c + a * a + b * b) / (2 * a * b)
     return cosa, cosb, cosc
 
+#Input
+#a, b, c - n x 3 vertex positions
+#Ai - area of corresponding triangle
+#Output
+#cot for each angle
 def find_cot(a, b, c, Ai):
     cota = (- a * a + b * b + c * c) / Ai
     cotb = (- b * b + a * a + c * c) / Ai
