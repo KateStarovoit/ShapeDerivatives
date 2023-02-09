e27c3bc52f93e64c939eafd39a928aac1ad54aeb
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-08 20:34:24 -0500

    Update move_vertex.py

--- kates_derivatives_draft/move_vertex.py
+++ kates_derivatives_draft/move_vertex.py
@@ -1,5 +1,9 @@
 import torch
 
 #move a vertex x0 along direction v 
+#Input
+#eps - scalar magnitude of translation
+#v - n x 3 tensor that gives direction
+#x0 - n x 3 tensor of vertex position
 def x_e(eps, x0, v):
     return x0 + eps*v
