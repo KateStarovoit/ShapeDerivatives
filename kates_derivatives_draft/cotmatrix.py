c0a084436bd1007e2c0efbc3e6b2142ce525d273
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-08 20:30:17 -0500

    Update cotmatrix.py

--- kates_derivatives_draft/cotmatrix.py
+++ kates_derivatives_draft/cotmatrix.py
@@ -3,13 +3,17 @@ import numpy as np
 import trig
 
 #Same as libigl cotmatrix. Computes Laplace matrix for a mesh
+#Input
+#V - n x 3 - list of vertex positions
+#F - m x 3 - list of faces
+#Output
+#n x n - Laplace matrix
 def cotmatrix(v, f):
     l = igl.edge_lengths(v,f)
     A = igl.doublearea(v,f)
     cot = np.zeros((len(l),3))
     for i in range(len(l)):
-        cota, cotb, cotc = find_cot(l[i][0], l[i][1], l[i][2], A[i])
-        cot[i] = [cota,cotb,cotc]
+        cot[i] = find_cot(l[i][0], l[i][1], l[i][2], A[i])
     L = np.zeros((len(v),(len(v))))
     for i in range(len(l)):
         for j in range(3):
