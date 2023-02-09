be5d641e3cc58e00fedfec27bc0e25e01bf09b4e
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-08 20:27:09 -0500

    Update cotmatrix_fd_directional_derivative.py

--- kates_derivatives_draft/cotmatrix_fd_directional_derivative.py
+++ kates_derivatives_draft/cotmatrix_fd_directional_derivative.py
@@ -1,8 +1,27 @@
-#import numpy as np
+import numpy as np
+import torch
 
-# dL/dx:d = 
+#Translates v along direction d
+#Input
+#v - n x 3 list or torch.tensor of vertex positions
+#d - n x 3 direction for each coordinte of each vertex
+#h - scalar - magnitude of translation
+#Output
+#n x 3 translated vertex positions
+def translate_shape(V,d,h):
+  return V + d*h
+
+#Input
+#v - n x 3 list or tensor of vertex positions
+#f - m x 3 list of faces
+#d - n x 3 direction for each coordinte of each vertex
+#h - scalar - magnitude of translation
+#Output
+#L_fd - finite difference approximation
 #  dl/dx:d = lim_{h->0}    (L(x +hd) - L(x))/h
 #    (L(x+hd) - L(x))//h
-def cotmatrix_fd_directional_derivative(V, F, d):
-  
-  return A 
+def cotmatrix_fd_directional_derivative(V, F, d, h):
+  V_ = translate_shape(V,d,h)
+  L = cotmatrix(V,F)    
+  L_ = cotmatrix(V_,F)
+  return (L_-L)/h
