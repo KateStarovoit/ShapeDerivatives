5e488938937507b20ed3daf5235a226b2ce006b6
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-09 16:06:49 -0500

    Update l_grad_entries.py

--- kates_derivatives_draft/l_grad_entries.py
+++ kates_derivatives_draft/l_grad_entries.py
@@ -51,3 +51,25 @@ def l_grad_entries_assembled(v,f):
                 for l in range(len(cot_entries[i][j][k])):
                       L_grad_assembled [i][j] += cot_entries[i][j][k][l] 
     return L_grad_assembled 
+
+def l_grad_entries(v,f):
+    L_grad_entries = []
+    A = igl.doublearea(v,f)
+    for i in range(len(f)):
+        L_grad_entries.append(grad_li([v[f[i][0]],v[f[i][1]],v[f[i][2]]],A[i]))
+    return L_grad_entries
+
+def l_diractional_derivative(v,f,d):
+    L_grad = np.zeros((len(v),(len(v))))
+    cot_entries = l_grad_entries(v,f)
+    for i in range(len(f)):
+        for j in range(3):
+           entry = 0
+           for k in range(len(cot_entries[i][j])):
+               for l in range(len(cot_entries[i][j][k])):
+                     entry += cot_entries[i][j][k][l] * d[f[i][j]][k]
+           L_grad[f[i][(j+1)%3]][f[i][(j+2)%3]]+= entry
+           L_grad[f[i][(j+2)%3]][f[i][(j+1)%3]]+= entry
+           L_grad[f[i][(j+1)%3]][f[i][(j+1)%3]]-= entry
+           L_grad[f[i][(j+2)%3]][f[i][(j+2)%3]]-= entry
+    return L_grad
