83cdb839d3f3a38d008701a6abced93ae064fc87
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-10 10:46:27 -0500

    Update example2.py

--- kates_derivatives_draft/examples/example2.py
+++ kates_derivatives_draft/examples/example2.py
@@ -1,12 +1,19 @@
-v, f = igl.read_triangle_mesh(os.path.join(root_folder, "data", "flag.obj"))
-b = np.zeros(len(v))
-b[4] = 1
-d = np.zeros((len(v),3))
-d[0] = [-1,0,0]
-h = 0.1
-print("fd approximation")
-print(cotmatrix_fd_directional_derivative(v, f, d, h))
-d = np.ones((len(v),len(v)))
-d[0][0] = -1
-print("directional derivative")
-print(cotmatrix_directional_derivative(v, f, d))
+import os
+from cotmatrix_fd_directional_derivative import *
+from cotmatrix_directional_dericative import *
+
+root_folder = os.getcwd()
+
+
+def example2():
+    v, f = igl.read_triangle_mesh(os.path.join(root_folder, "Data", "flag1.obj"))
+    b = np.zeros(len(v))
+    b[4] = 1
+    d = np.zeros((len(v),3))
+    d[0] = [0,0,0]
+    h = 0.1
+    print("fd approximation")
+    print(cotmatrix_fd_directional_derivative(v, f, d, h)[0])
+
+    print("directional derivative")
+    print(cotmatrix_directional_derivative(v, f, d)[0])
