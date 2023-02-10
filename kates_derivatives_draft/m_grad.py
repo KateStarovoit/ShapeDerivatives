2e5b6a38fd0e296e07fa5d1000a6ee1eb54e54dc
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-10 12:43:42 -0500

    Update m_grad.py

--- kates_derivatives_draft/m_grad.py
+++ kates_derivatives_draft/m_grad.py
@@ -1,13 +1,12 @@
-import torch
-
-#Gradient of the mass matrix
-#Input
-#V - n x 3 list or tensor of vertex positions
-#F - m x 3 ist or tensor of faces
-#Output
-#Mass matrix gradient
+from triangle_area import *
+'''Gradient of the mass matrix
+Input
+V - n x 3 list or tensor of vertex positions
+F - m x 3 ist or tensor of faces
+Output
+Mass matrix gradient'''
 def m_grad(v,f):
     M_grad = []
     for fi in f:
-         M_grad.append(torch.autograd.functional.jacobian(area,torch.tensor([v[fi[0]],v[fi[1]],v[fi[2]]])))
+         M_grad.append(torch.autograd.functional.jacobian(triangle_area,torch.tensor([v[fi[0]],v[fi[1]],v[fi[2]]])))
     return(M_grad)
