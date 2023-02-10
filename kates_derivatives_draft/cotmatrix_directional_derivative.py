2089d65e87357f3be62c0c5945bb119021c0d848
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-10 09:44:03 -0500

    Update cotmatrix_directional_derivative.py

--- kates_derivatives_draft/cotmatrix_directional_derivative.py
+++ kates_derivatives_draft/cotmatrix_directional_derivative.py
@@ -1,16 +1,24 @@
-#directional derivative for cot matrix
+from l_grad_entries import *
+
+
 '''
 Input
-V - n x 3vertex positions
+V - n x 3 vertex positions
 F - m x 3 faces
-d - n x n direction matrix
+d - n x 3 x 3 directions
 Output
-n x n cotmatrix directional derivative
-'''
-def cotmatrix_directional_derivative(V, F, d):
-    L_grad = l_grad(v,f)
-    print(len(L_grad), len(d))
-    for i in range(len(v)):
-        for j in range(len(v)):
-            L_grad[i][j] *= d[i][j]
+n x n cotmatrix directional derivative'''
+def cotmatrix_diractional_derivative(v,f,d):
+    L_grad = np.zeros((len(v),(len(v))))
+    cot_entries = l_grad_entries(v,f)
+    for i in range(len(f)):
+        for j in range(3):
+           entry = 0
+           for k in range(len(cot_entries[i][j])):
+               for l in range(len(cot_entries[i][j][k])):
+                     entry += cot_entries[i][j][k][l] * d[f[i][j]][k]
+           L_grad[f[i][(j+1)%3]][f[i][(j+2)%3]]+= entry
+           L_grad[f[i][(j+2)%3]][f[i][(j+1)%3]]+= entry
+           L_grad[f[i][(j+1)%3]][f[i][(j+1)%3]]-= entry
+           L_grad[f[i][(j+2)%3]][f[i][(j+2)%3]]-= entry
     return L_grad
