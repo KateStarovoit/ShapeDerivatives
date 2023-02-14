9e45abb4322ffbc2001849a3efaf1f7102530cf5
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-02-14 10:17:57 -0500

    Update l_grad_entries.py

--- kates_derivatives_draft/l_grad_entries.py
+++ kates_derivatives_draft/l_grad_entries.py
@@ -10,32 +10,16 @@ Output
 3 x 3 gradient of triangle
 '''
 def grad_li(v, A_i):
-    grad = [np.zeros((3,3)), np.zeros((3,3)), np.zeros((3,3))]
-    for i in range(len(grad)):
-        for j in range(0,3):
-            grad[i][j][0] = (2*v[i%3][j]-v[(i+1)%3][j]-v[(i+2)%3][j])/(4*A_i)
-            grad[i][j][1] = (v[(i+2)%3][j]-v[i%3][j])/(4*A_i)
-            grad[i][j][2] = (-v[(i+1)%3][j]-v[i%3][j])/(4*A_i)
+    grad = np.zeros((3,9))
+    for i in range(3):
+        k = 0
+        for j in range(3):
+            grad[i][k  + (i+2)%3] = (v[(i+1)%3][j] - v[(i)%3][j]) / (2*A_i)
+            grad[i][k + (i + 1)%3] = (-v[i][j] + v[(i+2)%3][j]) / ( 2*A_i)
+            grad[i][k + (i)%3] = (2 * v[(i)%3][j] - v[(i+1)%3][j] - v[(i+2)%3][j]) / ( 2*A_i)
+            k += 3
+    print(grad)
     return grad
-
-
-'''l_grad entries for each face in one direction
-Input
-V - n x 3 number of vertex positions
-F - m x 3 number of faces
-di - scalar 0, 1 or 2 - directon
-Output
-n x 3 list of (3 x 3) gradient for each face
-for each of 3 angles of n triangles (3 x 3) entries'''
-def l_grad_entries_di(v,f,di):
-    cot_entries = l_grad_entries(v,f)
-    L_grad_assembled = np.zeros((len(f),3))
-    for i in range(len(cot_entries)):
-        for j in range(len(cot_entries[i])):
-            for k in range(len(cot_entries[i][j])):
-                 L_grad_assembled [i][j] += cot_entries[i][j][di][k]
-    return L_grad_assembled
-
 
 '''l_grad entries for each face
 Input
@@ -51,21 +35,3 @@ def l_grad_entries(v,f):
     for i in range(len(f)):
         L_grad_entries.append(grad_li([v[f[i][0]],v[f[i][1]],v[f[i][2]]],A[i]))
     return L_grad_entries
-
-
-'''returns n*n l_grad_entries matrix
-Input
-V - n x 3 number of vertex positions
-F - m x 3 number of faces
-Output
-n x n list of entries for l_grad directional derivative
-'''
-def l_grad_entries_assembled(v,f):
-    cot_entries = l_grad_entries(v,f)
-    L_grad_assembled = np.zeros((len(f),3))
-    for i in range(len(cot_entries)):
-        for j in range(len(cot_entries[i])):
-            for k in range(len(cot_entries[i][j])):
-                for l in range(len(cot_entries[i][j][k])):
-                      L_grad_assembled [i][j] += cot_entries[i][j][k][l]
-    return L_grad_assembled
