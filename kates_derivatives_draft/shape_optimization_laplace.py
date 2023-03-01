6f9db1506143278b7ce6eb235dbba64162df423b
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-03-01 10:46:40 -0500

    Create shape_optimization_laplace.py

---
+++ kates_derivatives_draft/shape_optimization_laplace.py
@@ -0,0 +1,16 @@
+from torch_lu import *
+from gp_torch_utils import *
+
+def optimize(v, f, b, S, bc, h = 0.01, n_iter = 10):
+    v_opt = torch.tensor(v, requires_grad=True)
+    solver = LinearDirectSolve.apply
+    for _ in range(n_iter):
+        L = cotmatrix(v_opt, torch.tensor(f))
+        M = mass_matrix(v_opt, torch.tensor(f))
+        M_b = torch.sparse.mm(M, b.reshape(b.shape[0],1).double())
+        u = solver(L, torch.reshape(M_b,(-1,)))
+        E = torch.norm((u@S.double()-bc).sum())
+        E.backward()
+        E_grad = v_opt.grad
+        v_opt = (v_opt.detach() - h*E_grad).requires_grad_(True)
+    return v_opt
